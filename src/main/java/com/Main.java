43bddac40112ef8d20ffc5949589a5d408bbe8dc
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-04-09 10:08:23 +0900

    명언 CLI 생성

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -6,16 +6,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
+        int wiseSayingNumber = 0;
 
         while (true) {
             System.out.println("명령)");
             String cmd = scanner.nextLine().trim();
 
-            if (cmd.equals("종료"))
-            {
+            if (cmd.equals("종료")) {
                 System.out.println("프로그램을 종료합니다.");
 
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSayingContent = scanner.nextLine().trim();
+
+                System.out.print("작가 : ");
+                String wiseSayingAuthor = scanner.nextLine().trim();
+
+                System.out.println(++wiseSayingNumber + "번 명언이 등록되었습니다.");
             }
         }
     }
